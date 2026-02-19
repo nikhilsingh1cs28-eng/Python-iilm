c48bfc0d7596a95be15a62e57f99d9f9a9b03b8c
nikhilsingh1cs28-eng <nikhil.singh1.cs28@iilm.edu>
2026-02-19 11:31:13 +0530

    Create prime_number.py

---
+++ prime_number.py
@@ -0,0 +1,14 @@
+# prime_number.py
+# Check whether number is prime
+
+num = int(input("Enter a number: "))
+
+if num > 1:
+    for i in range(2, num):
+        if num % i == 0:
+            print("Not a prime number")
+            break
+    else:
+        print("Prime number")
+else:
+    print("Not a prime number")
